911baae88b0fb9c0cd2aa9c0fe7dd9536277c295
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2023-11-25 20:04:23 +0100

    Update checkbox animation. Make adjustments baesd on user stories. Refactored JavaScript

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Header related
 const headerMenus = document.querySelectorAll('.header-menu');
+const searchForm = document.querySelector('#search-bar');
 const alertBox = document.querySelector('#alert-box');
 const alertBoxToggle = document.querySelector('#alert-box-toggle');
 const dropdownMenu = document.querySelector('#menu-dropdown');
@@ -20,13 +21,14 @@ const setupProgressIndicator = document.querySelector('#setup-progress #progress
 const setupStepCheckboxes = document.querySelectorAll('.setup-step .check-marker .check input[type="checkbox"]');
 
 
+// ********** HELPER FUNCTIONS ********** //
 
 /**
  * Sets the aria-expanded accessibility attribute on an element.
  * @param {Element} el - The element to set the aria-expanded attribute on.
  * @param {boolean} value - The value to set the aria-expanded attribute to.
  */
-const setAriaExpanded = (el, value) => {
+function setAriaExpanded(el, value){
     if (typeof value !== 'boolean') throw new Error('Value must be a boolean');
     el.ariaExpanded = value;
 }
@@ -37,7 +39,7 @@ const setAriaExpanded = (el, value) => {
  * @param {Element} el  - The element to set the aria-checked attribute on.
  * @param {boolean} value  - The value to set the aria-checked attribute to.
  */
-const setAriaChecked = (el, value) => {
+function setAriaChecked(el, value){
     if (typeof value !== 'boolean') throw new Error('Value must be a boolean');
     el.ariaChecked = value;
 }
@@ -47,26 +49,166 @@ const setAriaChecked = (el, value) => {
  * Sets the aria-expanded accessibility attribute on an element based on whether it has the `show-flex` class.
  * @param {Element} el - The element to set the aria-expanded attribute on.
  */
-const setAriaExpandedByShowFlex = (el) => {
+function setAriaExpandedByShowFlex(el){
     if (el.classList.contains('show-flex')) {
         setAriaExpanded(el, true);
     }else{
         setAriaExpanded(el, false);
-    }
+    };
 }
+
+
+/**
+ * Update setup progress indicator and text.
+ * @returns {void}
+ */
+function updateProgress(){
+    let checkedCount = document.querySelectorAll('.setup-step .check-marker .check input:checked').length;
+    setupStepsCompleted.innerHTML = `${checkedCount}`;
+    let progress = (checkedCount / setupStepCheckboxes.length) * 100;
+    setupProgressIndicator.style.width = `${progress}%`;
+}
+
+
+// Set helper properties/functions on setup steps
+setupSteps.forEach((setupStep) => {
+    setupStep.isExpanded = () => {return !setupStep.classList.contains('close-setup-step')};
+
+    // Expands setup step and collapses all other setup steps that are open
+    setupStep.expand = function() { 
+        this.classList.remove('close-setup-step');
+        setAriaExpanded(this, true);
+        setupSteps.forEach(step => {
+            if (step !== this && step.isExpanded()) {
+                step.collapse();
+            };
+        }); 
+    };
+
+    // Collapse setup step
+    setupStep.collapse = function() { 
+        this.classList.add('close-setup-step');
+        setAriaExpanded(this, false);
+    };
+});
+
+
+// Set helper properties/functions on setup step checkboxes
+setupStepCheckboxes.forEach((checkbox) => {
+    // Set the index of each checkbox
+    checkbox.index = Array.from(setupStepCheckboxes).indexOf(checkbox);
+
+    // Gets the next unchecked checkbox from this checkbox, if any
+    checkbox.nextUncheckedCheckbox = function() {
+        // NOTE: "`this` checkbox" as used in context here, refers to the checkbox that this
+        // method is called on.
+
+        let nextUncheckedCheckbox = null;
+        let allChecked = Array.from(setupStepCheckboxes).every(checkbox => checkbox.checked);
+
+        // IF, all checkboxes are already checked, there is no next unchecked checkbox. Just return. 
+        if (allChecked) {
+            return nextUncheckedCheckbox;
+        };
+
+        // ELSE, continue with the rest of the function
+
+        // To get the next unchecked checkbox, We have to consider two approaches;
+        // 1. We can assume that the next unchecked checkbox is after `this` checkbox. Therefore, we start the loop 
+        //    from the index of next checkbox after `this` checkbox and check all checkboxes that come after it
+        //    until we find the next unchecked checkbox or reach the end of the NodeList.
+        // 
+        // 2. If the loop reaches the end of the NodeList and the next unchecked checkbox has not been found yet,
+        //    we can again assume that the next unchecked checkbox is before `this` checkbox, given that `this` checkbox 
+        //    is not the first checkbox in the Nodelist. Therefore, we start the loop from the beginning of the NodeList again
+        //    and check all checkboxes that come before `this` checkbox until we find the next unchecked checkbox 
+        //    or very unlikely, reach the index of `this` checkbox again.
+
+        // Before proceeding with to the loop, we define a function that checks if the next unchecked checkbox has been found.
+        // This function will take in a checkbox index, and return true if the following conditions are met:
+        // 1. The given checkbox index is greater than the index of `this` checkbox, 
+        // 2. The checkbox with the given index is unchecked. 
+        // Otherwise, it will return false.
+
+        let isNextUncheckedCheckbox = (checkboxIndex) => {
+            return (checkboxIndex > this.index) && !setupStepCheckboxes[checkboxIndex].checked;
+        };
+
+        // Taking the first approach
+        for(let currentCheckboxIndex = this.index + 1; currentCheckboxIndex < setupStepCheckboxes.length; currentCheckboxIndex++) {
+            if (isNextUncheckedCheckbox(currentCheckboxIndex)) {
+                nextUncheckedCheckbox = setupStepCheckboxes[currentCheckboxIndex];
+                break;
+            };
+
+            // If we have still not found the next unchecked and we have reached the end 
+            // Nodelist. We go with the second approach. 
+            if (this.index != 0 && currentCheckboxIndex == (setupStepCheckboxes.length - 1)) {
+
+                // Redefined the function that checks if the next unchecked checkbox has been found.
+                // The new adaptation of the function will take in a checkbox index, and return true if the following conditions are met:
+                // 1. The given checkbox index is less than the index of `this` checkbox,
+                // 2. The checkbox with the given index is unchecked.
+                // Otherwise, it will return false.
+
+                isNextUncheckedCheckbox = (checkboxIndex) => {
+                    return (checkboxIndex < this.index) && !setupStepCheckboxes[checkboxIndex].checked;
+                }
+
+                // Now start the loop from the beginning of the NodeList and stop at the index of `this` checkbox.
+                // To do this, set the loop counter to -1. 
+                // Why? 
+                // Well, after each loop, the loop counter is incremented by 1.
+                // Therefore, if the loop counter is set to -1, it will be incremented to 0 after this loop so that 
+                // the loop can start from the beginning(first checkbox) of the NodeList again.
+                // If the loop counter was to be set to 0, it will be incremented to 1 after this loop.
+                // and the loop will start from the second checkbox in the NodeList instead of the first one, after the reset.
+                currentCheckboxIndex = -1;
+            }
+        };
+        return nextUncheckedCheckbox;
+    }
+
+    checkbox.wasChecked = function() {
+        setAriaChecked(this, true);
+
+        let nextUncheckedCheckbox = this.nextUncheckedCheckbox();
+        if (nextUncheckedCheckbox) {
+            // Get the step that contains the next unchecked checkbox
+            let nextUncheckedCheckboxSetupStep = setupSteps[nextUncheckedCheckbox.index];
+            // Click on next uncompleted setup step to open it
+            nextUncheckedCheckboxSetupStep.click();
+        };
+    };
+
+    checkbox.wasUnchecked = function() {
+        setAriaChecked(this, false);
+    };
+});
+
+
+// ********** END HELPER FUNCTIONS ********** //
+
 
 
 document.body.onclick = (e) => {
     // Close header menus on outside click
     if (!alertBox.contains(e.target) && !alertBoxToggle.contains(e.target) && alertBox.classList.contains('show-flex')) {
         alertBox.classList.remove('show-flex');
-        setAriaExpandedByShowFlex(alertBoxToggle);
+        setAriaExpandedByShowFlex(alertBox);
     };
 
     if (!dropdownMenu.contains(e.target) && !dropdownMenuToggle.contains(e.target) && dropdownMenu.classList.contains('show-flex')) {
         dropdownMenu.classList.remove('show-flex');
-        setAriaExpandedByShowFlex(dropdownMenuToggle);
+        setAriaExpandedByShowFlex(dropdownMenu);
     };
+}
+
+
+// Prevent search form from submitting
+searchForm.onsubmit = (e) => {
+    e.stopImmediatePropagation();
+    e.preventDefault();
 }
 
 
@@ -117,7 +259,7 @@ closeSelectPlanModalBtns.forEach(btn => {
 
 // Expand/collapse setup guide on click
 mainSetupGuideOpener.onclick = () => {
-    mainSetupGuide.classList.toggle('show-flex');
+    mainSetupGuide.classList.toggle('hide');
     setAriaExpandedByShowFlex(mainSetupGuide);
 
     // Reset setup steps to initial state if setup guide is closed (Based on design prototype)
@@ -125,11 +267,9 @@ mainSetupGuideOpener.onclick = () => {
         setupSteps.forEach((setupStep, index) => {
             // Close all setup steps but leave the first one open
             if (index !== 0){
-                setupStep.classList.add('close-setup-step');
-                setAriaExpanded(setupStep, false);
+                setupStep.collapse();
             }else{
-                setupStep.classList.remove('close-setup-step');
-                setAriaExpanded(setupStep, true);
+                setupStep.expand();
             };
         });
     }
@@ -137,42 +277,23 @@ mainSetupGuideOpener.onclick = () => {
 
 
 // Expand/collapse setup steps on click or enter key press
-setupSteps.forEach(setupStep => {
+setupSteps.forEach((setupStep) => {
     let setupStepCheckbox = setupStep.querySelector('.check-marker .check input[type="checkbox"]');
 
-    const onOpen = () => {
-        setAriaExpanded(setupStep, true);
-
-        setupSteps.forEach(el => {
-            // Close all setup steps except the one that was opened
-            if (el !== setupStep && !el.classList.contains('close-setup-step')) {
-                el.classList.add('close-setup-step');
-                setAriaExpanded(el, false);
-            };
-        });
-    };
-
     setupStep.onclick = (e) => {
-        // Open setup step that was clicked
-
-        // Prevent setup step from opening if its checkbox was clicked and unchecked
-        if (setupStepCheckbox.contains(e.target) && !setupStepCheckbox.checked) {
-            return;
-        }
-        setupStep.classList.remove('close-setup-step');
-        onOpen();
+        // Expand setup step that was clicked anywhere except its checkbox, 
+        // if it is not already expanded
+        if (!setupStepCheckbox.contains(e.target) && !setupStep.isExpanded()){
+            setupStep.expand();
+        };
     };
 
     setupStep.onkeydown = (e) => {
         if (e.key === 'Enter') {
-            // Open setup step that received the enter key press
-
-            // Prevent setup step from opening if its checkbox was clicked and unchecked
-            if (setupStepCheckbox.contains(e.target) && !setupStepCheckbox.checked) {
-                return;
-            }
-            setupStep.classList.remove('close-setup-step');
-            onOpen();
+            // Open setup step that received Enter key anywhere except its checkbox
+            if (!setupStepCheckbox.contains(e.target) && !setupStep.isExpanded()){
+                setupStep.collapse();
+            };
         };
     };
     
@@ -180,41 +301,22 @@ setupSteps.forEach(setupStep => {
 
 
 // Check/uncheck setup step checkboxes on click or enter key press
-setupStepCheckboxes.forEach(checkbox => {
-    const onCheck = () => {
-        setAriaChecked(checkbox, true);
-
-        setupSteps.forEach(setupStep => {
-            // Open setup step that contains the checked checkbox and close all others
-            if (setupStep.contains(checkbox)) {
-                setupStep.classList.remove("close-setup-step");
-            }
-            // All other setup steps are closed automatically by the `onOpen()` function
-            // because the checkbox is inside the setup step, which will also receive the click event
-        });
-    };
-
-    const onUncheck = () => {
-        setAriaChecked(checkbox, false);
-    };
-
+setupStepCheckboxes.forEach((checkbox) => {
     checkbox.onclick = () => {
         if (checkbox.checked) {
-            onCheck();
+            checkbox.wasChecked();
         }else{
-            onUncheck();
+            checkbox.wasUnchecked();
         };
-
-        // Update progress indicator and steps completed
-        let checkedCount = document.querySelectorAll('.setup-step .check-marker .check input:checked').length;
-        setupStepsCompleted.innerHTML = `${checkedCount}`;
-        let progress = (checkedCount / setupStepCheckboxes.length) * 100;
-        setupProgressIndicator.style.width = `${progress}%`;
     };
 
     checkbox.onkeydown = (e) => {
         if (e.key === 'Enter') {
             checkbox.click();
         };
+    };
+
+    checkbox.onchange = () => {
+        updateProgress();
     };
 })
